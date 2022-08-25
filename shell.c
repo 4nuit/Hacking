4edebd8a35363a5e03e0fbed5c88fb1c40ac9c24
night <80219594+0x14mth3n1ght@users.noreply.github.com>
2022-08-25 16:34:03 +0200

    Add files via upload

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <stdlib.h>
+
+void main() {
+    char *name[2];
+
+    name[0] = "/bin/sh";
+    name[1] = NULL;
+    execve(name[0], name, NULL);
+    exit(0);
+}
