9b60d803ab544b638c182dcc3337e8bde86c6912
lol <lol@lol.com>
2024-11-12 16:41:15 +0100

    add sycl
    Former-commit-id: 14942b8e8f5bd258b7daa3efc0d663a3ac568de4

---
+++ hpc/hello_world/sycl/hello_buffer_gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --time=0:30:00
+#SBATCH --account=lxp
+#SBATCH --partition=gpu
+#SBATCH --qos=default
+#SBATCH --nodes=1
+
+module load env/staging/2023.1
+module load intel-oneapi
+module load 520nmx/20.4
+
+icpx -fsycl -fsycl-targets=nvptx64-nvidia-cuda hello_buffer_gpu.cpp -o  hello_buffer_gpu
+srun ./hello_buffer_gpu
