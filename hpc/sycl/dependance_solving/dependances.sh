bde6377ec6ebe8fb26eb6b32d7aa82f2f87781cc
lol <lol@lol.com>
2024-11-19 17:09:13 +0100

    add 1pwn doc + sycl dependancies ex (expressing parallelism, chap4)

--- hpc/sycl/dependance_solving/dependances.sh
+++ hpc/sycl/dependance_solving/dependances.sh
@@ -8,5 +8,5 @@
 module load env/staging/2023.1
 module load intel-oneapi
 
-icpx -fsycl -fsycl-targets=nvptx64-nvidia-cuda hello_buffer_gpu.cpp -o  hello_buffer_gpu
-srun ./hello_buffer_gpu
+make
+make run
