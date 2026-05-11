47483ba54d5b6b63c46aa6da940b439665903e85
night <night@none.com>
2026-05-11 02:42:28 +0200

    add bit utilities

---
+++ reverse/bit_utilities.py
@@ -0,0 +1,39 @@
+# https://github.com/keon/awesome-bits 								- useful formulas
+# https://en.wikipedia.org/wiki/Bitwise_operation#Boolean_algebra	- useful properties (cf MBA obfuscation)
+# https://rosettacode.org/wiki/Bitwise_operations#Python
+
+# x % (1 << n) == x & ((1 << n) - 1)
+
+def get_bit(i: signed_integer, x: bit):
+	return (i >> x) & 1
+
+def get_masked_value(i: signed_integer, x: bit):
+	return i & (1 << x)
+
+def signed2unsigned(i: unsigned_integer, n: bits):
+	"""
+	# 2^n - j , j = -i > 0
+	convert_positive = (1 << n ) + i
+	# avoid overflow for positive unsigned integers
+	return convert_positive % (1 << n)
+	"""
+	return i & ((1 << n) - 1)
+
+# prefer using hex values
+assert(5 == get_masked_value(5,2) + get_masked_value(5,1) + get_masked_value(5,0) == 4*get_bit(5,2) + 2*get_bit(5,1) + 1*get_bit(5,0) == 0b101)
+assert((signed2unsigned(-12,8) + 12) % (1<<8) == 0)
+
+
+def rol_n(i: signed_integer, r: rotations, n: bits):
+	i = signed2unsigned(i, n); r = r % n
+	max = (1 << n) - 1
+	num1 = (i << r) & max
+	num2 = (i >> (n - r)) & max
+	return num1 | num2
+
+def ror_n(i: signed_integer, r: rotations, n: bits):
+	i = signed2unsigned(i, n); r = r % n
+	max = (1 << n) - 1
+	num1 = (i >> r) & max
+	num2 = (i << (n - r)) & max
+	return num1 | num2
