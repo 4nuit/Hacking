81e7809cf87b623591cd4f2de8c6a26b84f9357e
night <night@none.com>
2026-06-20 23:21:23 +0200

    fix caesar , add crc32

--- prog/c_memo/caesar.c
+++ prog/c_memo/caesar.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void caesar(char *s){
-	int i;
+void caesar_base(char *s){
+	int i = 0;
 	// while s[i] is not '\0'
 	while (s[i]) {
 		// s[i] + 13 % 26
@@ -11,6 +11,28 @@ void caesar(char *s){
 			s[i] -= 13;
 		else
 			s[i] += 13;
+		i += 1;
+	}
+}
+
+void caesar(char *s){
+	int i = 0;
+	while (s[i]) {
+		// s[i] < 65 || s[i] > 90
+		if ( s[i] < 'A' || s[i] > 'Z'){
+
+			//  s[i] < 97 || s[i] > 122
+			if ( s[i] < 'a' || s[i] > 'z'){
+				//do nothing on non printable chars : 91 < c < 95
+			}
+			else{
+				//s[i] = (s[i] - 84) % 26 + 97
+				s[i] = (s[i] - ('a' - 13)) % 26 + 'a';
+			}
+		}
+		else
+			//s[i] = (s[i] - 52) % 26 + 65
+			s[i] = (s[i] - ('A' - 13)) % 26 + 'A';
 		i += 1;
 	}
 }
--- reverse/bit_utilities.py
+++ reverse/bit_utilities.py
@@ -1,4 +1,5 @@
 # https://rosettacode.org/wiki/Bitwise_operations#Python
+import zlib
 
 def get_bit(i: signed_integer, x: bit):
 	return (i >> x) & 1
@@ -19,6 +20,7 @@ def signed2unsigned(i: unsigned_integer, n: bits):
 assert(5 == get_masked_value(5,2) + get_masked_value(5,1) + get_masked_value(5,0) == 4*get_bit(5,2) + 2*get_bit(5,1) + 1*get_bit(5,0) == 0b101)
 assert((signed2unsigned(-12,8) + 12) % (1<<8) == 0)
 
+##### SHIFTS #####
 
 def rol_n(i: signed_integer, r: rotations, n: bits):
 	i = signed2unsigned(i, n); r = r % n
@@ -33,3 +35,33 @@ def ror_n(i: signed_integer, r: rotations, n: bits):
 	num1 = (i >> r) & mask
 	num2 = (i << (n - r)) & mask
 	return num1 | num2
+
+##### CRC32 #####
+
+def create_table():
+    a = []
+    for i in range(256):
+        k = i
+        for j in range(8):
+            if k & 1:
+                k ^= 0x1db710640
+            k >>= 1
+        a.append(k)
+    return a
+
+def crc_update(buf, crc):
+    crc ^= 0xffffffff
+    for k in buf:
+        crc = (crc >> 8) ^ crc_table[(crc & 0xff) ^ k]
+    return crc ^ 0xffffffff
+
+
+def CRC32(s):
+	return hex(zlib.crc32(s.encode()))
+
+string = "The quick brown fox jumps over the lazy dog"
+crc_table = create_table()
+enc1 = hex(crc_update(string.encode(), 0))
+enc2 = CRC32(string)
+
+assert(enc1 == enc2)
